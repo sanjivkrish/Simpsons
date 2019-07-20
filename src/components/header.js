8d381e25916b18a3b06dd85a4f9567e486ab084a
sanjivkrish <sanjiv436.k@gmail.com>
2019-07-20 23:11:07 +0200

    Create App header bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,31 @@
-import { Link } from "gatsby"
+import React from 'react';
 import PropTypes from "prop-types"
-import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+function Header({ siteTitle }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            {siteTitle}
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
-  </header>
-)
+  );
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+
+import Header from "../components/header"
 
 const IndexPage = () => (
-  <div>
-    Hello World
-  </div>
+  <>
+    <Header siteTitle="FUTURAMA" />
+  </>
 )
 
 export default IndexPage
