89388258f878e4e2731ffa0e146f1b977a296edc
sanjivkrish <sanjiv436.k@gmail.com>
2019-07-20 22:55:36 +0200

    Create service to handle character operations

---
+++ src/_services/character.service.js
@@ -0,0 +1,33 @@
+const config = {
+    baseUrl :  " futuramaapi.herokuapp.com",
+    characters : "/api/v2/characters",
+    quotes : "/api/characters/"
+}
+
+export const characterService = {
+    getAllCharacters,
+    getQuotesByCharacter
+};
+
+// Handle response for all API
+function handleResponse(response) {
+    return response.text().then(res => {
+        const data = JSON.parse(res);
+        
+        if (!response.ok) return Promise.reject(data || response.statusText);
+
+        return data;
+    });
+}
+
+function getAllCharacters() {
+    const targetURL = `${config.baseUrl}${config.characters}`;
+
+    return fetch(targetURL).then(handleResponse);
+}
+
+function getQuotesByCharacter(name) {
+    const targetURL = `${config.baseUrl}${config.quotes}${name}`;
+
+    return fetch(targetURL).then(handleResponse);
+}
