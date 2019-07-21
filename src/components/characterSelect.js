d7a838cec146ab6d43522da1e50ed013b200c07f
sanjivkrish <sanjiv436.k@gmail.com>
2019-07-21 11:24:54 +0200

    Update character select to auto-suggest options

--- src/components/characterSelect.js
+++ src/components/characterSelect.js
@@ -1,69 +1,341 @@
 import React from 'react';
-import { connect } from "react-redux"
-import { withStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Select from 'react-select';
+import { emphasize, withStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import NoSsr from '@material-ui/core/NoSsr';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 
 import { characterAction } from "../_actions/character.action"
 
 const styles = theme => ({
   root: {
     display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: "center"
-  },
-  formControl: {
+    justifyContent: 'center',
+},
+select: {
+    flexGrow: 1,
     margin: theme.spacing(4),
-    minWidth: 345,
+    maxWidth : 500,
+  },
+  input: {
+    display: 'flex',
+    padding: 0,
+    height: 'auto',
+  },
+  valueContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  chip: {
+    margin: theme.spacing(0.5, 0.25),
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === 'light' ? theme.palette.grey[300] : theme.palette.grey[700],
+      0.08,
+    ),
+  },
+  noOptionsMessage: {
+    padding: theme.spacing(1, 2),
+  },
+  singleValue: {
+    fontSize: 16,
+  },
+  placeholder: {
+    position: 'absolute',
+    left: 2,
+    bottom: 6,
+    fontSize: 16,
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing(1),
+    left: 0,
+    right: 0,
+  },
+  divider: {
+    height: theme.spacing(2),
   },
 });
 
-class CharacterSelect extends React.Component {
+function NoOptionsMessage(props) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.noOptionsMessage}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
 
+NoOptionsMessage.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  /**
+   * Props to be passed on to the wrapper.
+   */
+  innerProps: PropTypes.object,
+  selectProps: PropTypes.object.isRequired,
+};
+
+function inputComponent({ inputRef, ...props }) {
+  return <div ref={inputRef} {...props} />;
+}
+
+inputComponent.propTypes = {
+  inputRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({
+      current: PropTypes.any.isRequired,
+    }),
+  ]),
+};
+
+function Control(props) {
+  const {
+    children,
+    innerProps,
+    innerRef,
+    selectProps: { classes, TextFieldProps },
+  } = props;
+
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: classes.input,
+          ref: innerRef,
+          children,
+          ...innerProps,
+        },
+      }}
+      {...TextFieldProps}
+    />
+  );
+}
+
+Control.propTypes = {
+  /**
+   * Children to render.
+   */
+  children: PropTypes.node,
+  /**
+   * The mouse down event and the innerRef to pass down to the controller element.
+   */
+  innerProps: PropTypes.shape({
+    onMouseDown: PropTypes.func.isRequired,
+  }).isRequired,
+  innerRef: PropTypes.oneOfType([
+    PropTypes.oneOf([null]),
+    PropTypes.func,
+    PropTypes.shape({
+      current: PropTypes.any.isRequired,
+    }),
+  ]).isRequired,
+  selectProps: PropTypes.object.isRequired,
+};
+
+function Option(props) {
+  return (
+    <MenuItem
+      ref={props.innerRef}
+      selected={props.isFocused}
+      component="div"
+      style={{
+        fontWeight: props.isSelected ? 500 : 400,
+      }}
+      {...props.innerProps}
+    >
+      {props.children}
+    </MenuItem>
+  );
+}
+
+Option.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  /**
+   * props passed to the wrapping element for the group.
+   */
+  innerProps: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    key: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
+    onMouseMove: PropTypes.func.isRequired,
+    onMouseOver: PropTypes.func.isRequired,
+    tabIndex: PropTypes.number.isRequired,
+  }).isRequired,
+  /**
+   * Whether the option is focused.
+   */
+  isFocused: PropTypes.bool.isRequired,
+  /**
+   * Whether the option is selected.
+   */
+  isSelected: PropTypes.bool.isRequired,
+};
+
+function Placeholder(props) {
+  const { selectProps, innerProps = {}, children } = props;
+  return (
+    <Typography color="textSecondary" className={selectProps.classes.placeholder} {...innerProps}>
+      {children}
+    </Typography>
+  );
+}
+
+Placeholder.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  /**
+   * props passed to the wrapping element for the group.
+   */
+  innerProps: PropTypes.object,
+  selectProps: PropTypes.object.isRequired,
+};
+
+function SingleValue(props) {
+  return (
+    <Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
+      {props.children}
+    </Typography>
+  );
+}
+
+SingleValue.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  /**
+   * Props passed to the wrapping element for the group.
+   */
+  innerProps: PropTypes.any,
+  selectProps: PropTypes.object.isRequired,
+};
+
+function ValueContainer(props) {
+  return <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
+}
+
+ValueContainer.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  selectProps: PropTypes.object.isRequired,
+};
+
+function Menu(props) {
+  return (
+    <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
+      {props.children}
+    </Paper>
+  );
+}
+
+Menu.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.element.isRequired,
+  /**
+   * Props to be passed to the menu wrapper.
+   */
+  innerProps: PropTypes.object.isRequired,
+  selectProps: PropTypes.object.isRequired,
+};
+
+const components = {
+  Control,
+  Menu,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer,
+};
+
+class CharacterSelect extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
+
         this.state = {
-            activeCharacter : props.activeCharacter || ""
+            value : null
         }
     }
 
-    handleChange = (event) => {
-        const activeCharacter = event.target.value;
-        this.setState({ activeCharacter });
+    handleOnSelect = activeCharacter => {
+        this.setState({ value : activeCharacter.value })
 
         // Set active character in store
         this.props.dispatch({ type : "SET_ACTIVE_CHARACTER", activeCharacter })
+        this.props.dispatch(characterAction.getQuotesByCharacter(activeCharacter.value))
+    }
+    
+    handleOnChange = value => {
+        this.props.dispatch(characterAction.getCharactersByQuery(value))
     }
 
     componentDidMount = () => {
-        // Get character list from API
         this.props.dispatch(characterAction.getAllCharacters())
     }
 
     render() {
-        const { classes, characters } = this.props;
+        const  { classes, characters } = this.props;
+        const  { value } = this.state;
+
         if (!characters) return(<div></div>)
 
+        const characterList = characters.map(char => ({ 
+            label : char.Name,
+            value : char.Name,
+            Name : char.Name,
+            PicUrl : char.PicUrl
+        }))
+
         return (
-            <form className={classes.root} autoComplete="off">
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="selectCharacter">Select a Character</InputLabel>
-                    <Select
-                    value={this.state.activeCharacter}
-                    onChange={this.handleChange}
-                    inputProps={{
-                        name: 'activeCharacter',
-                        id: 'selectCharacter',
-                    }}
-                    >
-                        {
-                            characters.map(character => <MenuItem key={character.Name} value={character}>{character.Name}</MenuItem> )
-                        }
-                    </Select>
-                </FormControl>
-            </form>
+            <div className={classes.root}>
+                <div className={classes.select}>
+                    <NoSsr>
+                        <Select
+                        classes={classes}
+                        inputId="react-select-single"
+                        onInputChange={this.handleOnChange}
+                        TextFieldProps={{
+                            label: 'Select a Character',
+                            InputLabelProps: {
+                            htmlFor: 'react-select-single',
+                            shrink: true,
+                            },
+                        }}
+                        placeholder="Start typing your character..."
+                        options={characterList}
+                        components={components}
+                        value={value}
+                        onChange={this.handleOnSelect}
+                        />
+                    </NoSsr>
+                </div>
+            </div>
         )
     }
 }
